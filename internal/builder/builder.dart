67b30a9789a61e160452cba6fe638289085943a4
dymart <dylanmartin@ubiquitoustechnologies.io>
2021-04-14 13:33:07 -0700

    add path to external dart bin for compile

--- internal/builder/builder.dart
+++ internal/builder/builder.dart
@@ -4,14 +4,37 @@ main(List<String> args) async {
 
   String cmd = args[0];
 
+  // need to get the environment variable for dart find its path and use that here
+  Map<String, String> envVars = Platform.environment;
+
+  // error check this and if there is an error send message that says that
+  String? dartroot = envVars['DARTROOT'];
+  print("DARTROOT: ${dartroot}");
+
+  var dartexe = ""; 
+
+  if (Platform.isLinux) {
+    dartexe = "${dartroot}/bin/dart";
+
+  } else if (Platform.isMacOS) {
+    dartexe = "${dartroot}/bin/dart";
+
+  } else if (Platform.isWindows) {
+    dartexe = "${dartroot}/bin/dart.exe";
+
+  } else {
+    print("Platform not supported for environment variables");
+
+  }
+
   // Not all of these fucntions need to be implemented but it was a simple way to lay everything out to start
-  // As things get more complicated what happens is each switch statement should be put in its own seperate file and be called as a function
+  // As things get more complicated each switch statement should be put in its own seperate file and be called as a function
   // This is just a simple version to build up from
   // The build tool will have to be modified to take those files as a lib or something
 
   switch(cmd) { 
     case "compile": {
-      var result = await Process.run('dart',args);
+      var result = await Process.run(dartexe , args);
       stdout.write(result.stdout);
       stderr.write(result.stderr);
     } 
--- tests/README.md
+++ tests/README.md
@@ -11,5 +11,8 @@ bazel run //tests:hello
 and 
 
 This has dependencies on another library also defined in the repo
+still need to run from tests/package_simple:
+dart pub get
+before you run bazel to generate the files
 bazel build //tests/package_simple:example
 bazel run //tests/package_simple:example
